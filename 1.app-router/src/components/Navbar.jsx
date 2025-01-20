12c7c488dbd1a8bb0fafd4ca15a5069a308ce6d3
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-20 15:50:30 +0100

    Utiliser les liens Link

--- 1.app-router/src/components/Navbar.jsx
+++ 1.app-router/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <nav className="flex gap-x-2">
-      <Link className="underline" href="/">
+      <Link className={`underline ${pathname === "/" && "bg-red-600"}`} href="/">
         Accueil
       </Link>
-      <Link className="underline" href="/blog">
+      <Link className={`underline ${pathname === "/blog" && "bg-red-600"}`} href="/blog">
         Blog
       </Link>
-      <Link className="underline" href="/dashboards">
+      <Link className={`underline ${pathname === "/dashboards" && "bg-red-600"}`} href="/dashboards">
         Dashboards
       </Link>
-      <Link className="underline" href="/contact">
+      <Link className={`unederline ${pathname === "/contact" && "bg-red-600"}`} href="/contact">
         Contact
       </Link>
     </nav>
