46da3c95e17b2a5beb29dede8609fb56f8e31a75
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-22 15:25:46 +0100

    Les routes dynamiques

--- 1.app-router/src/app/blog/[id]/page.jsx
+++ 1.app-router/src/app/blog/[id]/page.jsx
@@ -1,4 +1,21 @@
-function page() {
-  return <div>page</div>;
+import Link from "next/link";
+
+async function page({ params }) {
+  const { id } = await params;
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const post = await res.json();
+  console.log(post);
+
+  return (
+    <main className="mt-12">
+      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+      <p className="text-gray-600 mb-8">{post.body}</p>
+
+      <Link href="/blog" className="text-blue-500 hover:underline">
+        ← Retour à la page de blog
+      </Link>
+    </main>
+  );
 }
 export default page;
