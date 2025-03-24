0fbe62d9442d0eb8bcb2e39f68aac06f8364dd83
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-24 14:45:02 +0100

    feat: Server Action dans un composant client

--- 2.composants-et-donnees/src/app/form/page.jsx
+++ 2.composants-et-donnees/src/app/form/page.jsx
@@ -1,9 +1,15 @@
 "use client";
 import { useState } from "react";
+import { getPost } from "@/lib/serverActions";
 
 function page() {
   const [inputValue, setInputValue] = useState("");
-  console.log(new Date().getTime());
+  // console.log(new Date().getTime());
+
+  async function handleGetArticle() {
+    const post = await getPost(1);
+    console.log(post);
+  }
 
   return (
     <div>
@@ -13,7 +19,11 @@ function page() {
         className="p-2 mb-4 border border-gray-600"
         type="text"
       />
-      <p>Tu as écris : {inputValue}</p>
+      <p>Tu as écrit : {inputValue}</p>
+
+      <button className="p-4 bg-green-500" onClick={handleGetArticle}>
+        Récupérer un article
+      </button>
     </div>
   );
 }
