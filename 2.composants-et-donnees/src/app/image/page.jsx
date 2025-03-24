313c096ac5412e6aaac94e4e74433a7a8f22b0f2
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-24 11:42:39 +0100

    feat: creer un composant server

--- 2.composants-et-donnees/src/app/image/page.jsx
+++ 2.composants-et-donnees/src/app/image/page.jsx
@@ -1,13 +1,13 @@
 async function page() {
+  // Simule un appel API pour illustrer l’usage de fetch (sans traiter une réponse JSON)
   const res = await fetch("https://placehold.co/600x400");
-
-  const imgObject = res;
+  const imgObject = res.url;
   console.log(imgObject);
 
   return (
     <div>
       <h1>Une image</h1>
-      <img src={imgObject.url} alt="" />
+      <img src={imgObject} alt="Image de test" />
     </div>
   );
 }
