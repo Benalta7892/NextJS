7a97a80835e38c3564179aea8a68c8f14863db2b
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-26 11:44:33 +0100

    feat: optimisation des images

--- 3.styles-et-optimisation/src/app/page.js
+++ 3.styles-et-optimisation/src/app/page.js
@@ -1,10 +1,22 @@
 import Card from "@/components/Card/Card";
+import Image from "next/image";
+import mountain from "../../public/images/mountain-1080.jpg";
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-5xl mx-auto">
       <h1 className="text-2xl font-mono mb-6">Styles et optimisation</h1>
       <Card />
+      <Image
+        src={mountain}
+        alt="Une montagne"
+        sizes="
+        (max-width: 500px) 500px,
+        (max-width: 800px) 800px,
+        (max-width: 1080px) 1080px,
+        1080px
+      "
+      />
     </main>
   );
 }
