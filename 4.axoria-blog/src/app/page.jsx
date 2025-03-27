3ab0bfd86f44ffbdb8a1cfc218bb052f005cc023
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-27 19:54:27 +0100

    feat: creation de la base de donnees et connexion

--- 4.axoria-blog/src/app/page.jsx
+++ 4.axoria-blog/src/app/page.jsx
@@ -1,4 +1,6 @@
+import { connect } from "mongoose";
 import Link from "next/link";
+import { connectToDB } from "./lib/utils/db/connectToDB";
 
 const posts = [
   {
@@ -16,6 +18,8 @@ const posts = [
 ];
 
 export default async function Home() {
+  await connectToDB();
+
   return (
     <div className="u-main-container u-padding-content-container">
       <h1 className="t-main-title">Stay up to date with AXORIA.</h1>
@@ -24,16 +28,27 @@ export default async function Home() {
       <p className="mr-4 text-md to-zinc-900">Latest articles</p>
       <ul className="u-articles-grid">
         {posts.map((post, id) => (
-          <li className="rounded-sm shadow-md hover:shadow-xl border hover:border-zinc-300">
+          <li
+            key={id}
+            className="rounded-sm shadow-md hover:shadow-xl border hover:border-zinc-300"
+          >
             <div className="pt-5 px-5 pb-7">
               <div className="flex items-baseline gap-x-4 text-xs">
-                <time dateTime={new Date().toISOString()} className="text-gray-500 text-sm">
-                  {new Date().toLocaleDateString("en-EN", { year: "numeric", month: "long", day: "numeric" })}
+                <time
+                  dateTime={new Date().toISOString()}
+                  className="text-gray-500 text-sm"
+                >
+                  {new Date().toLocaleDateString("en-EN", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
                 </time>
                 <Link
                   href={`/categories/author/${post.autho}`}
                   className="ml-auto text-base text-gray-700
-                  hover:text-gray-600 whitespace-nowrap truncate">
+                  hover:text-gray-600 whitespace-nowrap truncate"
+                >
                   {post.author}
                 </Link>
               </div>
@@ -41,7 +56,8 @@ export default async function Home() {
                 href={`/articles/${post.title}`}
                 className="inline-block mt-6
               text-xl font-semibold text-zinc-800 hover:text-zinc-600
-              ">
+              "
+              >
                 {post.title}
               </Link>
             </div>
