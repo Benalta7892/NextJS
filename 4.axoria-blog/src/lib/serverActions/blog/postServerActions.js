c8e6ebaaceea6c14a17ccad54d23ff3e808cc7e7
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-28 14:39:48 +0100

    feat: creation de server action addPost

---
+++ 4.axoria-blog/src/lib/serverActions/blog/postServerActions.js
@@ -0,0 +1,32 @@
+"use server";
+
+import { connectToDB } from "@/lib/utils/db/connectToDB";
+
+export async function addPost(formData) {
+  // On extrait les données du formulaire (destructuring grace aux names des inputs et textarea)
+  const { title, markdownArticle } = Object.fromEntries(formData);
+
+  // try catch pour gérer les erreurs lors de la création du post
+  try {
+    // On attend la connexion à la base de données ou on utilise la connexion existante
+    await connectToDB();
+
+    // On definit le modèle de données "Post" pour creer une instance de post
+    const newPost = new Post({
+      title,
+      markdownArticle,
+    });
+
+    // On sauvegarde le post dans la base de données
+    const savedPost = await newPost.save();
+    console.log("Post saved");
+
+    // On retourne un objet avec un message de succès et le slug du post
+    return { success: true, slug: savedPost.slug };
+
+    // On catch les erreurs et on les affiche dans la console
+  } catch (err) {
+    console.log("Error while creating the post :", err);
+    throw new Error(err.message || "An error occured while creating the post");
+  }
+}
