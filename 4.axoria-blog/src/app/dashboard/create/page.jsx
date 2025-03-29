d3e5ae082ed81cc5f3e6d07f97906821a2d5303a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-29 16:20:55 +0100

    feat: coder l'interface de l'ajout de tags

--- 4.axoria-blog/src/app/dashboard/create/page.jsx
+++ 4.axoria-blog/src/app/dashboard/create/page.jsx
@@ -1,7 +1,11 @@
 "use client";
 import { addPost } from "@/lib/serverActions/blog/postServerActions";
+import { useState, useRef } from "react";
 
 function page() {
+  const [tags, setTags] = useState(["css", "javascript"]);
+  const tagInputRef = useRef(null);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -13,6 +17,10 @@ function page() {
 
     const result = await addPost(formData);
   };
+
+  const handleAddTag = () => {};
+
+  const handleRemoveTag = (tag) => {};
 
   return (
     <main className="u-main-container bg-white p-7 mt-32 mb-44">
@@ -31,6 +39,45 @@ function page() {
           placeholder="Title"
           required
         />
+
+        <div className="mb-10">
+          <label className="f-label" htmlFor="tag">
+            Add a tag(s) (optional, max 5)
+          </label>
+          <div className="flex">
+            <input
+              type="text"
+              className="shadow border rounded p-3 text-gray-700 focus:outline-slate-400"
+              id="tag"
+              placeholder="Add a tag"
+              ref={tagInputRef}
+            />
+            <button
+              className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold p-4 rounded mx-4"
+              onClick={handleAddTag}
+              type="button"
+            >
+              Add
+            </button>
+            <div className="flex items-center grow whitespace-nowrap overflow-y-auto shadow border rounded px-3">
+              {tags.map((tag, id) => (
+                <span
+                  key={id}
+                  className="inline-block whitespace-normal bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2"
+                >
+                  {tag}
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveTag(tag)}
+                    className="text-red-500 ml-2"
+                  >
+                    &times;
+                  </button>
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
 
         <label htmlFor="markdownArticle" className="f-label">
           Write your article using markdown - do not repeat the already given
