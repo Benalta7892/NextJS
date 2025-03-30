8663ff26d59333b438b77097e32d4be8c83f5740
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-30 21:12:43 +0200

    feat: gerer les infos liees a l'ajout d'un article

--- 4.axoria-blog/src/app/dashboard/create/page.jsx
+++ 4.axoria-blog/src/app/dashboard/create/page.jsx
@@ -1,15 +1,21 @@
 "use client";
 import { addPost } from "@/lib/serverActions/blog/postServerActions";
+import { useRouter } from "next/navigation";
 import { useState, useRef } from "react";
 
 function page() {
   const [tags, setTags] = useState([]);
+  const router = useRouter();
   const tagInputRef = useRef(null);
+  const submitButtonRef = useRef(null);
+  const serverValidationText = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Les donnees de notre formulaire
     const formData = new FormData(e.target);
+
     // On met tags dans le formData
     // On utilise JSON.stringify pour convertir le tableau en chaîne de caractères
     formData.set("tags", JSON.stringify(tags));
@@ -19,7 +25,33 @@ function page() {
       console.log(key, value);
     }
 
-    const result = await addPost(formData);
+    serverValidationText.current.textContent = "";
+    submitButtonRef.current.textContent = "Saving Post...";
+    submitButtonRef.current.disabled = true;
+
+    try {
+      const result = await addPost(formData);
+
+      if (result.success) {
+        submitButtonRef.current.textContent = "Post Saved ✅";
+
+        let countdown = 3;
+        serverValidationText.current.textContent = `Redirecting in ${countdown}...`;
+        const interval = setInterval(() => {
+          countdown -= 1;
+          serverValidationText.current.textContent = `Redirecting in ${countdown}...`;
+
+          if (countdown === 0) {
+            clearInterval(interval);
+            router.push(`/article/${result.slug}`);
+          }
+        }, 1000);
+      }
+    } catch (error) {
+      submitButtonRef.current.textContent = "Submit";
+      serverValidationText.current.textContent = `${error.message}`;
+      submitButtonRef.current.disabled = false;
+    }
   };
 
   const handleAddTag = () => {
@@ -76,22 +108,16 @@ function page() {
             <button
               className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold p-4 rounded mx-4"
               onClick={handleAddTag}
-              type="button"
-            >
+              type="button">
               Add
             </button>
             <div className="flex items-center grow whitespace-nowrap overflow-y-auto shadow border rounded px-3">
               {tags.map((tag, id) => (
                 <span
                   key={id}
-                  className="inline-block whitespace-normal bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2"
-                >
+                  className="inline-block whitespace-normal bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2">
                   {tag}
-                  <button
-                    type="button"
-                    onClick={() => handleRemoveTag(tag)}
-                    className="text-red-500 ml-2"
-                  >
+                  <button type="button" onClick={() => handleRemoveTag(tag)} className="text-red-500 ml-2">
                     &times;
                   </button>
                 </span>
@@ -101,14 +127,9 @@ function page() {
         </div>
 
         <label htmlFor="markdownArticle" className="f-label">
-          Write your article using markdown - do not repeat the already given
-          title
+          Write your article using markdown - do not repeat the already given title
         </label>
-        <a
-          href="https://www.markdownguide.org/cheat-sheet/"
-          target="_blank"
-          className="block mb-4 text-blue-600"
-        >
+        <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank" className="block mb-4 text-blue-600">
           How to use the markdown syntax ?
         </a>
 
@@ -117,15 +138,15 @@ function page() {
           id="markdownArticle"
           className="min-h-44 text-xl shadow appearance-none border rounded
             w-full p-8 to-gray-700 mb-4 focus:outline-slate-400"
-          required
-        ></textarea>
+          required></textarea>
 
         <button
+          ref={submitButtonRef}
           className="min-w-44 bg-indigo-500 hover:bg-indigo-700
-          text-white font-bold py-3 px-4 rounded border-none mb-4"
-        >
+          text-white font-bold py-3 px-4 rounded border-none mb-4">
           Submit
         </button>
+        <p ref={serverValidationText}></p>
       </form>
     </main>
   );
