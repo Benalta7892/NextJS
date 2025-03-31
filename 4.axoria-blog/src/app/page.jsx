91f13d77167194c061d3b7a4d87532096568fb73
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-03-31 17:15:36 +0200

    feat: creer la methode lisant le cookie d'authentification

--- 4.axoria-blog/src/app/page.jsx
+++ 4.axoria-blog/src/app/page.jsx
@@ -19,10 +19,7 @@ import { getPosts } from "@/lib/serverMethods/blog/postMethods";
 // ];
 
 export default async function Home() {
-  await connectToDB();
-
   const posts = await getPosts();
-  console.log("posts", posts);
 
   return (
     <div className="u-main-container u-padding-content-container">
@@ -32,16 +29,10 @@ export default async function Home() {
       <p className="mr-4 text-md to-zinc-900">Latest articles</p>
       <ul className="u-articles-grid">
         {posts.map((post, id) => (
-          <li
-            key={id}
-            className="rounded-sm shadow-md hover:shadow-xl border hover:border-zinc-300"
-          >
+          <li key={id} className="rounded-sm shadow-md hover:shadow-xl border hover:border-zinc-300">
             <div className="pt-5 px-5 pb-7">
               <div className="flex items-baseline gap-x-4 text-xs">
-                <time
-                  dateTime={new Date().toISOString()}
-                  className="text-gray-500 text-sm"
-                >
+                <time dateTime={new Date().toISOString()} className="text-gray-500 text-sm">
                   {new Date().toLocaleDateString("en-EN", {
                     year: "numeric",
                     month: "long",
@@ -51,8 +42,7 @@ export default async function Home() {
                 <Link
                   href={`/categories/author/johndoe`}
                   className="ml-auto text-base text-gray-700
-                  hover:text-gray-600 whitespace-nowrap truncate"
-                >
+                  hover:text-gray-600 whitespace-nowrap truncate">
                   John Doe
                 </Link>
               </div>
@@ -60,8 +50,7 @@ export default async function Home() {
                 href={`/article/${post.slug}`}
                 className="inline-block mt-6
               text-xl font-semibold text-zinc-800 hover:text-zinc-600
-              "
-              >
+              ">
                 {post.title}
               </Link>
             </div>
