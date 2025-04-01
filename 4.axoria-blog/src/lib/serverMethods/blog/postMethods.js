2fb15733bd52d1f7d56796ebc4882070cdfb2fc0
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-04-01 15:23:08 +0200

    feat: gerer les erreurs des methodes serveurs

--- 4.axoria-blog/src/lib/serverMethods/blog/postMethods.js
+++ 4.axoria-blog/src/lib/serverMethods/blog/postMethods.js
@@ -1,29 +1,24 @@
 import { Post } from "@/lib/models/post";
 import { connectToDB } from "@/lib/utils/db/connectToDB";
 import { Tag } from "@/lib/models/tag";
+import { notFound } from "next/navigation";
 
 export const getPost = async (slug) => {
-  try {
-    await connectToDB();
+  await connectToDB();
 
-    const post = await Post.findOne({ slug }).populate({
-      path: "tags",
-      select: "name slug",
-    });
-    console.log("post", post);
+  const post = await Post.findOne({ slug }).populate({
+    path: "tags",
+    select: "name slug",
+  });
 
-    return post;
-  } catch (err) {
-    console.error("Error while fetch a post", err);
-    throw new Error("Failed to fetch post");
-  }
+  if (!post) return notFound();
+
+  return post;
 };
 
 export const getPosts = async () => {
-  try {
-    await connectToDB();
-    const posts = await Post.find({});
+  await connectToDB();
+  const posts = await Post.find({});
 
-    return posts;
-  } catch (err) {}
+  return posts;
 };
