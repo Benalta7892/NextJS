9c6c84512bef4cfb6575ebd92c5b4946f719e1db
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-04-03 12:03:30 +0200

    feat: coder l'UI de la page categorie

--- 4.axoria-blog/src/app/categories/page.jsx
+++ 4.axoria-blog/src/app/categories/page.jsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
 import { getTags } from "@/lib/serverMethods/blog/tagMethods";
 
 const page = async () => {
   const tags = await getTags();
   console.log(tags, "tagsConsole");
 
-  return <div>page</div>;
+  return (
+    <main className="u-main-container u-padding-content-container">
+      <h1 className="t-main-title">All categories</h1>
+      <p className="t-main-subtitle">Find articles sorted by category tag.</p>
+
+      <ul className="u-articles-grid">
+        {tags.length > 0 ? (
+          tags.map((tag) => (
+            <li key={tag._id} className="bg-gray-100 border rounded shadow-md">
+              <Link href={`/categories/tag/${tag.slug}`} className="flex p-4 pb-6 items-baseline">
+                <span className="text-lg font-semibold underline">#{tag.name}</span>
+                <span className="ml-auto">
+                  Articles count : <span className="font-semibold">{tag.postCount}</span>
+                </span>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <li>No categories found.</li>
+        )}
+      </ul>
+    </main>
+  );
 };
 export default page;
